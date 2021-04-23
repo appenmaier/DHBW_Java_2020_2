51aeb1f04848bdb38f924e7bfac9c6c054f2fa09
appenmaier <daniel.appenmaier@gmail.com>
2021-04-23 12:47:29 +0200

    ...

---
+++ DHBW_Java_2020_2/src/chapter07/demos/demo01/Demo0701.java
@@ -0,0 +1,41 @@
+package chapter07.demos.demo01;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Aufbau einer JavaFX-Anwendung
+ * 
+ * @author Daniel Appenmaier
+ *
+ */
+public class Demo0701 extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void init() throws Exception {
+		// Auslesen und Verarbeiten der Eingabeparameter
+		System.out.println("init()");
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		// Aufbau der Bühne, Aufbau der Szene(n) und Programmlogik
+		System.out.println("start()");
+		View view = new View();
+		Scene scene = new Scene(view.getRoot());
+		primaryStage.setTitle("Demo 7.1");
+		primaryStage.setHeight(500);
+		primaryStage.setWidth(500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public void stop() throws Exception {
+		// "Aufräumarbeiten"
+		System.out.println("stop()");
+	}
+
+}
---
+++ DHBW_Java_2020_2/src/chapter07/demos/demo01/View.java
@@ -0,0 +1,39 @@
+package chapter07.demos.demo01;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class View {
+
+	/*
+	 * Attribute
+	 */
+	// Bildschirmelemente / Controls
+	private TextField inputTextField;
+	private Button inputToOutputButton;
+	private Label outputLabel;
+	// Container
+	VBox root;
+
+	/*
+	 * Methoden
+	 */
+	public View() {
+		inputTextField = new TextField();
+		inputTextField.setPromptText("Eingabefeld");
+
+		inputToOutputButton = new Button("Eingabe zu Ausgabe");
+
+		outputLabel = new Label("Ausgabefeld");
+
+		root = new VBox();
+		root.getChildren().addAll(inputTextField, inputToOutputButton, outputLabel);
+	}
+
+	public VBox getRoot() {
+		return root;
+	}
+
+}
